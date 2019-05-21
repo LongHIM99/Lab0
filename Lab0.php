0c7c8c1984bed38f1af56423f675581b29242655
LongHIM99 <50312623+LongHIM99@users.noreply.github.com>
2019-05-21 09:59:29 +0700

    marge

--- Lab0.php
+++ Lab0.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Lab 0</h1>
 
+	<?php
 
-?>
+		$i=1;
+		while($1<=5){
+			echo "The number is ".$i."<br />";
+		}
+		
+	?>
+</body>
+</html>
